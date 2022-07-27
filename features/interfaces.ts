efde5fa32837caf953aff976df3159917e218a9d
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-07-27 12:04:38 -0500

    End of section 7 interfaces

---
+++ features/interfaces.ts
@@ -0,0 +1,31 @@
+interface Reportable {
+  // name: string;
+  // year: number;
+  // broken: boolean;
+  summary(): string;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: 2000,
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
+};
+
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink is ${this.color}`;
+  },
+};
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(drink);
