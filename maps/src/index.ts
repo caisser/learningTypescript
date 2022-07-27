8b610e1c9c8d5f10a1dae22de8d21b41759b62fe
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-07-27 16:31:31 -0500

    Creating a map part 1

---
+++ maps/src/index.ts
@@ -0,0 +1,9 @@
+/// <reference types="@types/google.maps" />
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+
+console.log(user);
+console.log(company);
