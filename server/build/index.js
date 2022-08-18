d3bec493200afa9b1853de7d0c97753937897747
Luis David Cardona Calderon <luis.cardona@globant.com>
2022-08-18 17:52:40 -0500

    End of server to start decorators

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const loginRoutes_1 = require("./routes/loginRoutes");
+const cookie_session_1 = __importDefault(require("cookie-session"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json({ limit: '10kb' }));
+app.use(express_1.default.urlencoded({ extended: true, limit: '10kb' }));
+app.use((0, cookie_session_1.default)({ keys: ['kakakaka'] }));
+app.use(loginRoutes_1.router);
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieSession({ keys: ['kakakaka'] }));
+
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
